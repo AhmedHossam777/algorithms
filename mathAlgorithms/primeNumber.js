c41939a6fe8b243d5beea15342361333aa6a6e11
Ahmed Hossam <ahmedhossam9226@gmail.com>
2024-02-08 22:49:46 +0200

    solve prime number problem

---
+++ mathAlgorithms/primeNumber.js
@@ -0,0 +1,15 @@
+//! given a number 'n' determine if the number is prime or not
+function isPrime(n) {
+  if (n <= 1) {
+    return false;
+  }
+  for (let i = 2; i * i <= n; i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true
+}
+console.log(isPrime(5));
+console.log(isPrime(4));
+console.log(isPrime(13));
