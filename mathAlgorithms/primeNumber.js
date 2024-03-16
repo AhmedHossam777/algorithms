aad37c9fe10145a9b49e33f0a7075769d1304f24
Ahmed Hossam <ahmedhossam9226@gmail.com>
2024-03-17 01:24:14 +0200

    solve primeNumber problem

--- mathAlgorithms/primeNumber.js
+++ mathAlgorithms/primeNumber.js
@@ -11,5 +11,20 @@ function isPrime(n) {
   return true
 }
 console.log(isPrime(5));
-console.log(isPrime(4));
-console.log(isPrime(13));
+console.log(isPrime(8));
+
+
+// function isPrime(n) {
+//   if (n <= 1) {
+//     return false;
+//   }
+//   for (let i = 2; i * i <= n; i++) {
+//     if (n % i === 0) {
+//       return false;
+//     }
+//   }
+//   return true
+// }
+// console.log(isPrime(5));
+// console.log(isPrime(4));
+// console.log(isPrime(13));
