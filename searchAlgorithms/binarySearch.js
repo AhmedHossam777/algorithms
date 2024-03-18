74d77f82d271c701d18d8166d46f6df425cf3d2f
Ahmed Hossam <ahmedhossam9226@gmail.com>
2024-03-18 17:59:16 +0200

    binary search algorithm

--- searchAlgorithms/binarySearch.js
+++ searchAlgorithms/binarySearch.js
@@ -1,26 +1,23 @@
 //! problem : given an array of 'n' element and a target 't' element, find the index if 't' return -1 if not found
 const arr = [1, 2, 3, 4, 8, 6, 7, 9];
 
-const findNum = (arr, target) => {
+const binarySearch = (arr, target) => {
   let leftIndex = 0;
   let rightIndex = arr.length - 1;
 
   while (leftIndex <= rightIndex) {
     let middleIndex = Math.floor((leftIndex + rightIndex) / 2);
-
     if (target === arr[middleIndex]) return middleIndex;
 
-    if (target > arr[middleIndex]) {
-      leftIndex = middleIndex + 1;
-    } else {
+    if (target < arr[middleIndex]) {
       rightIndex = middleIndex - 1;
+    } else {
+      leftIndex = middleIndex + 1;
     }
   }
-  return -1
 };
 
+console.log(binarySearch(arr, 9));
 
-console.log(findNum(arr, 4)); // 3
-console.log(findNum(arr, 9)); // 7
-
+//? Binary search work only for sorted array
 //? Big-o = O(logn)
